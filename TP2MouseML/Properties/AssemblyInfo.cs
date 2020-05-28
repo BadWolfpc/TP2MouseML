bf8903877358c3d8e072c641c4dc3c17ad318358
BadWolf <34199091+BadWolfpc@users.noreply.github.com>
2020-05-28 03:42:31 -0400

    Add files via upload

---
+++ TP2MouseML/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(TP2MouseML.TP2MouseML.BuildInfo.Name)]
+[assembly: AssemblyDescription(TP2MouseML.TP2MouseML.BuildInfo.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(TP2MouseML.TP2MouseML.BuildInfo.Company)]
+[assembly: AssemblyProduct(TP2MouseML.TP2MouseML.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + TP2MouseML.TP2MouseML.BuildInfo.Author)]
+[assembly: AssemblyTrademark(TP2MouseML.TP2MouseML.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(TP2MouseML.TP2MouseML.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(TP2MouseML.TP2MouseML.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(TP2MouseML.TP2MouseML), TP2MouseML.TP2MouseML.BuildInfo.Name, TP2MouseML.TP2MouseML.BuildInfo.Version, TP2MouseML.TP2MouseML.BuildInfo.Author, TP2MouseML.TP2MouseML.BuildInfo.DownloadLink)]
+[assembly: MelonModGame(TP2MouseML.TP2MouseML.BuildInfo.CompatibleGame, TP2MouseML.TP2MouseML.BuildInfo.CompatibleGame)]
---
+++ TP2MouseML/TP2MouseML.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+
+namespace TP2MouseML
+{
+	public class TP2MouseML : MelonMod
+	{
+        public static class BuildInfo
+        {
+            public const string CompatibleGame = "VRChat";
+            public const string Name = "TP2MouseML";
+            public const string Description = null;
+            public const string Author = "CJ";
+            public const string Company = null;
+            public const string Version = "1.0.0";
+            public const string DownloadLink = null;
+        }
+
+        public override void OnApplicationStart()
+		{
+            MelonModLogger.Log(ConsoleColor.Cyan, "==============================================");
+            MelonModLogger.Log(ConsoleColor.Green, "TP2Mouse by CJ [Loaded!]");
+            MelonModLogger.Log(ConsoleColor.Green, "Press T to teleport to your crosshair!");
+            MelonModLogger.Log(ConsoleColor.Cyan, "==============================================");
+		}
+
+        public override void OnGUI()
+        {
+        }
+
+		public override void OnUpdate()
+		{
+            if (Input.GetKeyDown(KeyCode.T))
+			{
+                RayTeleport();
+            }
+        }
+
+        public static void RayTeleport()
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            if (hits.Length > 0)
+            {
+                RaycastHit raycastHit = hits[0];
+                VRCPlayer.field_Internal_Static_VRCPlayer_0.transform.position = raycastHit.point;
+            }
+        }
+    }
+}
